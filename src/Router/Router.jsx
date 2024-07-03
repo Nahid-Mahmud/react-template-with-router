1ce0be74f5d58a08d2eb7282445c3cf9c8581a00
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-07-03 23:21:44 +0600

    Default element style changed

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -9,8 +9,9 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: (
-          <h1 className="font-bold text-5xl flex h-screen items-center justify-center">
-            This is a react Template (React + React Router Dom + Tailwind Css + Axios + TANSTACK query + TANSTACK dev tool )
+          <h1 className="font-bold p-5  max-w-7xl  text-4xl mx-auto flex h-screen items-center justify-center">
+            This is a react Template (React + React Router Dom + Tailwind Css + Axios + TANSTACK query + TANSTACK dev
+            tool )
           </h1>
         ),
       },
